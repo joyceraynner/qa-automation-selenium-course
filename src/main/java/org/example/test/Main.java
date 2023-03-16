959f2602f3c5fb29b9f0eb1166dbba195d510453
joyceraynner <joyceraynner48@gmail.com>
2023-03-16 16:51:58 -0300

    Add message for git test 2

--- src/main/java/org/example/test/Main.java
+++ src/main/java/org/example/test/Main.java
@@ -2,7 +2,7 @@ package org.example.test;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("git test");
+        System.out.println("git test2");
 
     }
 
